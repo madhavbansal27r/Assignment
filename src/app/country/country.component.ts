122d3abf780f8b75fc24f5c0f702758ce76aebcd
Madhav Bansal <madhavbansal27r@gmail.com>
2023-11-24 23:12:30 +0530

    Transition

--- src/app/country/country.component.ts
+++ src/app/country/country.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { appModuleAnimation } from '@shared/animations/routerTransition';
 
 @Component({
   selector: 'app-country',
   templateUrl: './country.component.html',
-  styleUrls: ['./country.component.css']
+  styleUrls: ['./country.component.css'],
+  animations: [appModuleAnimation()]
+
 })
 export class CountryComponent {
 
--- src/app/state/state.component.ts
+++ src/app/state/state.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { appModuleAnimation } from '@shared/animations/routerTransition';
+
 
 @Component({
   selector: 'app-state',
   templateUrl: './state.component.html',
-  styleUrls: ['./state.component.css']
+  styleUrls: ['./state.component.css'],
+  animations: [appModuleAnimation()]
+
 })
 export class StateComponent {
 
