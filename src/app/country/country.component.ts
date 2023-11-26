de2c1cde2f35b9f38c898e8d488977baf3346a3c
Vikas <vikasdhamija9914@gmail.com>
2023-11-27 00:37:53 +0530

    Ui_for_county

--- src/app/country/country.component.ts
+++ src/app/country/country.component.ts
@@ -1,97 +1,118 @@
 import { Component, Injector, OnInit } from '@angular/core';
 import { appModuleAnimation } from '@shared/animations/routerTransition';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
-import { CreateCountryComponent } from './create-country/create-country.component';
-import { EditCountryComponent } from './edit-country/edit-country.component';
-import { AppComponentBase } from '@shared/app-component-base';
-import { UserServiceProxy } from '@shared/service-proxies/service-proxies';
-import { CountryServiceProxy } from '@shared/service-proxies/service-proxies';
+// import { CreateCountryComponent } from './create-country/create-country.component';
+// import { EditCountryComponent } from './edit-country/edit-country.component';
+// import { AppComponentBase } from '@shared/app-component-base';
+import { CountryDto, CountryDtoPagedResultDto, CountryServiceProxy, UserServiceProxy } from '@shared/service-proxies/service-proxies';
+// import { CustomApiService } from '@shared/service-proxies/service-proxies';
+import { PagedListingComponentBase, PagedRequestDto } from '@shared/paged-listing-component-base';
+import { finalize } from 'rxjs/operators';
 
 // import {
 //   PagedListingComponentBase,
 //   PagedRequestDto
 // } from 'shared/paged-listing-component-base';
+
+class PagedCompanysRequestDto extends PagedRequestDto {
+  keyword: string;
+
+}
 @Component({
   selector: 'app-country',
   templateUrl: './country.component.html',
   styleUrls: ['./country.component.css'],
   animations: [appModuleAnimation()]
-
 })
-export class CountryComponent extends AppComponentBase implements OnInit {
-  Countries: any;
-  tempdata = [
-    { "id": 1, "name": "John" },
-    { "id": 2, "name": "Jane" },
-    { "id": 3, "name": "Alice" },
-    { "id": 4, "name": "Bob" },
-    { "id": 5, "name": "Eve" },
-    { "id": 6, "name": "Charlie" },
-    { "id": 7, "name": "David" },
-    { "id": 8, "name": "Emily" },
-    { "id": 9, "name": "Frank" },
-    { "id": 10, "name": "Grace" }
-  ];
+export class CountryComponent extends PagedListingComponentBase<CountryDto> {
+  countrys: CountryDto[] = [];
+  keyword = '';
+  isActive: boolean | null;
+  advancedFiltersVisible = false;
+ 
 
-  constructor(private _modalService: BsModalService, Injector: Injector, public _userService: UserServiceProxy,public _countryservice:CountryServiceProxy) {
-    super(Injector);
-  }
-  ngOnInit(): void {
-    // call the service to fetch all country
-    // this._countryservice.getAll().subscribe((data: any) => {
-    //   this.Countries = data;
-    //   // this.showPaging(result, pageNumber);
-    // });
-    // this.Countries = this.tempdata;
-  }
-  createcountry() {
-    this.showCreateOrEditUserDialog();
+  constructor(
+    injector: Injector,
+    private _countryService: CountryServiceProxy,
+    private _modalService: BsModalService
+  ) {
+    super(injector);
   }
 
-  protected delete(id: any): void {
+  createCountry(): void {
+    // this.showCreateOrEditCountryDialog();
+  }
+
+  editCountry(country: CountryDto): void {
+   // this.showCreateOrEditCountryDialog(country.id);
+  }
+
+  clearFilters(): void {
+    this.keyword = '';
+    this.getDataPage(1);
+  }
+
+  protected list(
+    request: PagedCompanysRequestDto,
+    pageNumber: number,
+    finishedCallback: Function
+  ): void {
+    request.keyword = this.keyword;
+
+    this._countryService
+      .getAll(
+        request.keyword,
+        request.skipCount,
+        request.maxResultCount
+      )
+      .pipe(
+        finalize(() => {
+          finishedCallback();
+        })
+      )
+      .subscribe((result: CountryDtoPagedResultDto) => {
+        this.countrys = result.items;
+        this.showPaging(result, pageNumber);
+      });
+  }
+
+  protected delete(country: CountryDto): void {
     abp.message.confirm(
-      this.l('UserDeleteWarningMessage', id),
+      this.l('UserDeleteWarningMessage', country.name),
       undefined,
       (result: boolean) => {
         if (result) {
-          this._userService.delete(id).subscribe(() => {
+          this._countryService.delete(country.id).subscribe(() => {
             abp.notify.success(this.l('SuccessfullyDeleted'));
-            // this.refresh();
+            this.refresh();
           });
         }
       }
     );
   }
-
-  editUser(id: any) {
-    this.showCreateOrEditUserDialog(id);
-  }
-  private showCreateOrEditUserDialog(id?: number): void {
-    let createOrEditUserDialog: BsModalRef;
+  private showCreateOrEditUserDialog(id?: string): void {
+    let createOrEditCountryDialog: BsModalRef;
     if (!id) {
-      createOrEditUserDialog = this._modalService.show(
-        CreateCountryComponent,
-        {
-          class: 'modal-lg',
-        }
-      );
+      // createOrEditUserDialog = this._modalService.show(
+      //   CreateUserDialogComponent,
+      //   {
+      //     class: 'modal-lg',
+      //   }
+      // );
     } else {
-      createOrEditUserDialog = this._modalService.show(
-        EditCountryComponent,
-        {
-          class: 'modal-lg',
-          initialState: {
-            id: id,
-          },
-        }
-      );
+      // createOrEditUserDialog = this._modalService.show(
+      //   EditUserDialogComponent,
+      //   {
+      //     class: 'modal-lg',
+      //     initialState: {
+      //       id: id,
+      //     },
+      //   }
+      // );
     }
 
-
-
-
-    // createOrEditUserDialog.content.onSave.subscribe(() => {
-    //   this.refresh();
-    // });
+    createOrEditCountryDialog.content.onSave.subscribe(() => {
+      this.refresh();
+    });
   }
 }
