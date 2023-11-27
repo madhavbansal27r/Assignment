4d0323dce0cd213ea11ef0f8292a4226ba2a683f
Madhav Bansal <madhavbansal27r@gmail.com>
2023-11-27 16:43:37 +0530

    state create

--- src/app/state/create-state/create-state.component.ts
+++ src/app/state/create-state/create-state.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Injector, OnInit, Output } from '@angular/core';
+import { AppComponentBase } from '@shared/app-component-base';
+import { CountryDto, CountryServiceProxy, StateDto, StateServiceProxy } from '@shared/service-proxies/service-proxies';
+import { BsModalRef } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-create-state',
   templateUrl: './create-state.component.html',
   styleUrls: ['./create-state.component.css']
 })
-export class CreateStateComponent {
+export class CreateStateComponent extends AppComponentBase implements OnInit {
 
+  @Output() onSave = new EventEmitter<any>();
+  constructor(public bsModalRef: BsModalRef, public _countryservice: CountryServiceProxy, public _stateservice: StateServiceProxy, injector: Injector,) {
+    super(injector);
+  }
+  saving = false;
+
+
+  state = new StateDto();
+  finalstate = new StateDto();
+  countrydata: any;
+  ngOnInit(): void {
+    this.getallcountry();
+
+  }
+
+  getallcountry(): void {
+    this._countryservice.getcountrydata().subscribe((countrydata: any) => {
+      this.countrydata = countrydata.result.items;
+    });
+  }
+
+  gettingvalues() {
+    let statename = this.state.sName; // this is ok
+    let selectedcountryname = this.state.countryName; // assuming you have a property in 'state' for the selected country code
+
+  }
+
+
+  save(): void {
+    this.saving = true;
+    this.gettingvalues();
+    this._stateservice.create(this.finalstate).subscribe(
+      () => {
+        this.notify.info(this.l('SavedSuccessfully'));
+        this.bsModalRef.hide();
+        this.onSave.emit();
+      },
+      () => {
+        this.saving = false;
+      }
+    );
+  }
 }
